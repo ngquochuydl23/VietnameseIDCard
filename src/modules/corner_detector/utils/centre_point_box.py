74ba52d64d631116c43dda32382548d5520411cc
unknown <nguyenquochuydl123@gmail.com>
2025-03-13 08:55:38 +0700

    Finish stage 1

---
+++ src/modules/corner_detector/utils/centre_point_box.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+def get_centre_point_box(box):
+    x_min, y_min, x_max, y_max = box.xyxy[0].tolist()
+    x_center = (x_min + x_max) / 2
+    y_center = (y_min + y_max) / 2
+    
+    return x_center, y_center
+
+
+def get_centre_point_boxex(boxes):
+    centre_points = []
+    for box in boxes:
+        x, y = get_centre_point_box(box)
+        centre_points.append((x, y))
+        print(f'Center: ({x}, {y})')
+        
+    return centre_points
+        
+def draw_centre_points(original_img, points):
+    cv2_img = np.array(original_img)
+
+    # Convert RGB to BGR (for OpenCV)
+    cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_RGB2BGR)
+    for point in points:
+        cv2.circle(cv2_img, (int(point[0]), int(point[1])), radius=5, color=(255, 0, 0), thickness=-1)
+    return cv2_img
---
+++ src/modules/corner_detector/utils/preprocessing.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # Tổng của x + y --> top-left có tổng nhỏ nhất, bottom-right có tổng lớn nhất
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]  # top-left
+    rect[2] = pts[np.argmax(s)]  # bottom-right
+
+    # Hiệu x - y --> top-right có hiệu nhỏ nhất, bottom-left có hiệu lớn nhất
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]  # top-right
+    rect[3] = pts[np.argmax(diff)]  # bottom-left
+
+    return rect
+
+def warp_image_with_centres(origin_img, centre_points, output_size=(800, 600)):
+    width, height = output_size
+    pts = np.array(centre_points, dtype="float32")
+
+    ordered_pts = order_points(pts)
+    dst_pts = np.array([
+        [0, 0],
+        [width - 1, 0],
+        [width - 1, height - 1],
+        [0, height - 1]
+    ], dtype="float32")
+    
+    # Tính ma trận biến đổi và warp
+    M = cv2.getPerspectiveTransform(ordered_pts, dst_pts)
+    return cv2.warpPerspective(origin_img, M, (width, height))
